1b441c0630a1f38a1e2825d7a797b050245ff36c
gondiski <enochotieno@rocketmail.com>
2020-11-03 18:34:51 +0300

    Generation of serializers for student and course resources

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :course
+  validates :full_name, presence: true
+  validates :time_preference, presence: true
+  validates :email, presence: true
 end
